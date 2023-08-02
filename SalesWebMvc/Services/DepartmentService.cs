c3540f3b1ca5fcd7b845bd943c849183f1eeea1d
felipe-esposito <68409648+felipe-esposito@users.noreply.github.com>
2023-08-01 22:32:15 -0300

    Asynchronous operations

--- SalesWebMvc/Services/DepartmentService.cs
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -1,20 +1,20 @@
-﻿using SalesWebMvc.Data;
-using SalesWebMvc.Models;
+﻿using SalesWebMvc.Models;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
 
 namespace SalesWebMvc.Services
 {
     public class DepartmentService
     {
         private readonly SalesWebMvcContext _context;
-
         public DepartmentService(SalesWebMvcContext context)
         {
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
